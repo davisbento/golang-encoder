71ce8a2b0148abec564379337b1d910c94efbc0e
davisbento <contato@davisbento.dev>
2023-02-21 15:11:03 -0300

    upload manager goroutines

--- application/services/upload_manager.go
+++ application/services/upload_manager.go
@@ -3,9 +3,11 @@ package services
 import (
 	"cloud.google.com/go/storage"
 	"context"
+	log "github.com/sirupsen/logrus"
 	"io"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 )
 
@@ -60,6 +62,63 @@ func (manager *UploadManager) LoadPaths() error {
 	}
 
 	return nil
+}
+
+func (manager *UploadManager) ProcessUpload(concurrency int, doneUpload chan string) error {
+	in := make(chan int, runtime.NumCPU()) // channel to send the index of the paths
+	returnChannel := make(chan string)     // channel to receive the result of the upload
+
+	err := manager.LoadPaths()
+	if err != nil {
+		return err
+	}
+
+	client, ctx, err := getClientUpload()
+	if err != nil {
+		return err
+	}
+
+	// start the workers with goroutines
+	for process := 0; process < concurrency; process++ {
+		go manager.uploadWorker(in, returnChannel, client, ctx)
+	}
+
+	// send the index of the paths to the workers
+	go func() {
+		for x := 0; x < len(manager.Paths); x++ {
+			in <- x
+		}
+		close(in)
+	}()
+
+	// keep listening the return channel to check if any error occurs
+	for r := range returnChannel {
+		if r != "success" {
+			// if any error occurs, we stop the upload
+			doneUpload <- r
+			log.Println("Upload failed")
+			break
+		}
+
+		log.Println("Upload success")
+	}
+
+	return nil
+}
+
+func (manager *UploadManager) uploadWorker(in chan int, returnChannel chan string, uploadClient *storage.Client, ctx context.Context) {
+	for x := range in {
+		err := manager.UploadObject(manager.Paths[x], uploadClient, ctx)
+		if err != nil {
+			manager.Errors = append(manager.Errors, manager.Paths[x])
+			log.Printf("Error uploading file %v. Error: %v", manager.Paths[x], err)
+			returnChannel <- err.Error()
+		}
+
+		returnChannel <- "success"
+	}
+
+	returnChannel <- "finished"
 }
 
 func getClientUpload() (*storage.Client, context.Context, error) {
